528b4e20c1ffcc1f7c278c3649398b2e193916f1
zhangqy@feelingtech.net <zhangqy@feelingtech.net>
2024-04-18 17:13:19 +0800

    init: 配置husky

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 import routerInstance from './router';
-var testA = 0
+let testA = 0
+
+
 const piniaInstance = createPinia()
 const appInstance = createApp(App)
 appInstance
